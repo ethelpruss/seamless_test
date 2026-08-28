13909110cd5b81ca6d3bd0692074d937ba003f3b
Ethel Pruss <47779830+ethelpruss@users.noreply.github.com>
2026-08-28 15:52:46 +0200

    added database structure info for backup

---
+++ Database/database_schema.sql
@@ -0,0 +1,229 @@
+
+-- Tables for Statistics 2 lectures
+
+CREATE TABLE statistics2_module1 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module2 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module3 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module4 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module5 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module6 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module7 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module8 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module9 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module10 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module11 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE statistics2_module12 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+-- Tables for Complex Systems lectures
+
+CREATE TABLE complexsystems_module1 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module2 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module3 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module4 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module5 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module6 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module7 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module8 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module9 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module10 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module11 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module12 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
+
+CREATE TABLE complexsystems_module13 (
+    user_id   TEXT NOT NULL,
+    event     TEXT NOT NULL,
+    label     TEXT NOT NULL,
+    correct   TEXT NOT NULL,
+    question  TEXT NOT NULL,
+    answer    TEXT NOT NULL
+);
